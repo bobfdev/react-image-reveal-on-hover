84a0cf601ef01afff179c2a6c5beb0bf4136755f
Bob F <bobfdev@gmail.com>
2022-04-03 22:45:43 -0500

    initial files

---
+++ src/components/Animation.js
@@ -0,0 +1,14 @@
+import {
+    Container,
+    Title
+} from './StyledAnimation';
+
+const Animation = () => {
+    return (
+        <Container>
+            <Title>Detroit Style Pizza types</Title>
+        </Container>
+    )
+}
+
+export default Animation;
---
+++ src/components/StyledAnimation.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+    width: 100%;
+    height: 100%;
+`
+
+export const Title = styled.h1`
+    font-size: 45px;
+    letter-spacing: 4px;
+    text-align: center;
+    color: rgb(234, 131, 34);
+`
